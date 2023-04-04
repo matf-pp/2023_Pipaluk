979fa56c719b7adf34c0aa7f90a926bc21e4ae95
VukAmidzic <vuk.amidzic@gmail.com>
2023-04-04 10:31:07 +0200

    Tilemap logic

--- src/level.rs
+++ src/level.rs
@@ -8,6 +8,11 @@ use sdl2::video::WindowContext;
 use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::mouse::Cursor;
+
+const TILEMAP_WIDTH: i32 = 100;
+const TILEMAP_HEIGHT: i32 = 100;
+const TILE_OFFSET: i32 = 1;
 
 extern crate sdl2;
 
@@ -39,18 +44,18 @@ pub fn play_level(
 ) -> GameResult {
     let level = load_level("resources/levels/".to_string() + name + ".json");
 
-    for row in level.tilemap {
+    /*for row in &level.tilemap {
         for cell in row {
             print!("{}", cell);
         }
         println!();
-    }
-    // println!("{}", level.tilemap[0][0]);
+    }*/
+    //println!("{}", level.tilemap[0][0]);
 
     // let level_name = &level["name"];
     // let tilemap = &level["tilemap"];
 
-    let scale = 4;
+    let scale = 3;
 
     let mut img_floor = texture_creator.load_texture("resources/images/floor.png").unwrap();
     img_floor.set_blend_mode(BlendMode::Blend);
@@ -58,13 +63,44 @@ pub fn play_level(
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} => { return GameResult::Quit }
+                Event::Quit {..}
+                | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => { return GameResult::Quit },
                 _ => {}
             }
         }
+        canvas.set_draw_color(Color::BLACK);
         canvas.clear();
 
-        // canvas.copy(&img_floor, None, Rect::new(0, 0, 28*scale, 19*scale)).unwrap();
+        for i in 0..level.tilemap.len() {
+            for j in 0..level.tilemap[i].len() {
+                if i % 2 == 0 {
+                    canvas.copy(
+                        &img_floor, 
+                        None, 
+                        Rect::new(
+                            100 + (((i+1) as i32))*(28/2)*scale as i32,
+                            100 + (((j+1) as i32))*(19/2 + 5)*scale as i32,
+                            28 * scale, 
+                            19 * scale,
+                        )
+                    )
+                    .unwrap();
+                }
+                else {
+                    canvas.copy(
+                        &img_floor, 
+                        None, 
+                        Rect::new(
+                            100 + ((i as i32))*(28/2)*scale as i32 + 40,  
+                            100 + ((j as i32))*(19/2 + 5)*scale as i32 + 20, 
+                            28 * scale, 
+                            19 * scale,
+                        )
+                    )
+                    .unwrap();
+                }
+            }
+        }
         canvas.present();
     }
 
